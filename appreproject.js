e3b3e22b3d6c369c6df9d3dbeecf3c99afe136fe
iceboxik <kirilltim21@gmail.com>
2024-11-09 00:44:46 +0300

    Добавил выпадающий список в самом верху страницы

--- appreproject.js
+++ appreproject.js
@@ -2,8 +2,12 @@ const arrow = document.getElementById('textOnClick');
 const table = document.getElementById('openspisok');
 const displayForm = document.getElementById('omagad');
 const congratulations = document.getElementById('congratulationsModelWindowForJs');
-const buttonsForForms = document.querySelectorAll("#formFirst,#formSecond")
-
+const buttonsForForms = document.querySelectorAll("#formFirst,#formSecond");
+const sectionMenu = document.getElementById('sectionMenuForJs');
+const menu = document.getElementById('menuForJs');
+const burgerButton = document.getElementById('burgerButton');
+const burgerButtonDiv = document.getElementById('burgerButtonDiv');
+const menuLinks = document.querySelectorAll('.menu_links')
 
 document.getElementById('openspisok').addEventListener('click', function(event){
 
@@ -51,11 +55,13 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
         congratulations.style.display = 'none';
+        document.getElementById('dataFromForJS').reset();
     }
 }
 document.getElementById('closeModalWindow').onclick = function () {
     displayForm.style.display = 'none';
     congratulations.style.display = 'none';
+    document.getElementById('dataFromForJS').reset();
 }
 
 document.getElementById('dataFromForJS').onsubmit = function(event){
@@ -64,9 +70,46 @@ document.getElementById('dataFromForJS').onsubmit = function(event){
     document.getElementById('dataFromForJS').reset();
 }
 
+burgerButton.onclick = function(){
+    if (getComputedStyle(sectionMenu).display == 'none') {
+        sectionMenu.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        document.getElementById('blackoutJs').style.display = 'block'
+    }
+    else{
+        sectionMenu.style.display = 'none';
+        document.body.style.overflow = '';
+        document.getElementById('blackoutJs').style.display = 'none'
+    }
+    
+    
+}
+
+window.onclick = function(event){
+    if (event.target != burgerButton & !burgerButton.contains(event.target) & !sectionMenu.contains(event.target)) {
+        sectionMenu.style.display = 'none';
+        document.body.style.overflow = '';
+        document.getElementById('blackoutJs').style.display = 'none'
+    }
+}
 
 
 
+function updateSize(){
+    const block1Rect = burgerButtonDiv.getBoundingClientRect();
+    sectionMenu.style.top = `${block1Rect.bottom + 20}px`; // $ позволяет производить мат действия в ковычках
+    sectionMenu.style.left = `${block1Rect.left - 200}px`;
+}
 
+updateSize();
 
+window.addEventListener('resize', updateSize);
 
+menuLinks.forEach(function(item){//item - каждый элемент из массива
+    item.addEventListener('click', function(){
+        sectionMenu.style.display = 'none';
+        document.getElementById('blackoutJs').style.display = 'none'
+        document.body.style.overflow = '';
+
+    })
+})
